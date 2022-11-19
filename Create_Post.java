7c2b27218a2dd9ca16e7ec5250ab1f2abd36de0e
gsklavounakos <116825713+gsklavounakos@users.noreply.github.com>
2022-11-19 14:51:52 +0200

    Add files via upload
    createpost file

---
+++ Create_Post.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class CreatePost
+
+    public static String newPost() {
+        	Scanner in = new Scanner(System.in);
+            System.out.print.ln("Write your post:");
+            String post = in.nextLine();
+            return post;
+    }
