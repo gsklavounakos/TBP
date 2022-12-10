fa2eed93c445ca285b65492da84f7b4ab2bbaa63
EffieVorylla <118600898+EffieVorylla@users.noreply.github.com>
2022-12-10 23:11:27 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,49 +1,44 @@
-import java.util.Scanner;
+mport java.util.Scanner;
+import static java.lang.System.exit;
+import java.util.InputMismatchException;
 
 public class Main {
 
 	public static void main(String args[]) {
-  		Scanner in = new Scanner(System.in);
-  		boolean wrongRequest;
+  		boolean read = false;
   		System.out.println("Welcome to ToEater!");
-
-  		do {
-
-			wrongRequest = false;
-
-			System.out.println("Enter: " + "\n" +
+		System.out.println("Enter: " + "\n" +
                         "\r" + "1.Register" + "\n" +
                         "\r" + "2.Log in" + "\n" +
                         "\r" + "3.Exit.");
-			int option = in.nextInt();
-
-			switch(option) {
-				case 1:
-					Register r = new Register();
-					r.registerNewUser();
-					System.out.println("Enter: " + "\n" +
-                        "\r" + "1.Personal account" + "\n" +
-                        "\r" + "2.Professional account");
-					int typeAccount = in.nextInt();
-					break;
-				case 2:
-					Login l = new Login();
-					l.signInUser();
-					break;
-				case 3:
-					break;
-				default:
-					System.out.println("This option doesn't exist, choose 1, 2 or 3");
-					wrongRequest = true;
-					break;
-				}
-
-		}while(wrongRequest == true);
+        int option = 0;
+		while (read != true) {
+			try{
+				Scanner in = new Scanner(System.in);
+				option = in.nextInt();
+				read = true;
+			}catch (InputMismatchException e) {
+				read = false;
+			}
+			if (option != 1 & option != 2 & option != 3) {
+				read = false;
+				System.err.println("This option doesn't exist, choose 1, 2 or 3");
+			}
+		}
+		switch(option) {
+			case 1:
+				Register r = new Register();
+				r.registerNewUser();
+				break;
+			case 2:
+				Login l = new Login();
+				l.signInUser();
+				break;
+			case 3:
+				exit(0);
+		}
 	}
-
 }
-
-
 
 
 
