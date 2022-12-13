008519979bba0487191c0fbaa63a34b3abd938d7
gveriop <117642649+gveriop@users.noreply.github.com>
2022-12-13 03:33:05 +0200

    Add files via upload

--- Posts.java
+++ Posts.java
@@ -1,53 +1,36 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 
 public class Posts {
-    ArrayList<String> allPosts = new ArrayList<String>();
-    ArrayList<Integer> likes = new ArrayList<Integer>();
 
-    public void addPost(){
-        CreatePost x = new createPost();
-        String y= x.newPost();
-        allPosts.add(y);
-    }
+    Scanner in = new Scanner(System.in);
 
-    public void likePost(int i){
-        likes(i)==0;
-
-    }
-
-    public void showSinglePost(int i){
-        System.out.println(allPosts(i));
-
-    }
-    
     public void showPosts(){
-        
-        if (allPosts.size() =! 0) {
-            Scanner in = new Scanner(System.in);
-            int y=0;
-            showSinglePost(y);
-            do {
-                System.out.println("Press: (1) to show Next Post, (2) to Like this post, (3) to go back to menu")
-                int option = in.nextInt();
-
-                switch(option) {
-                    case 1: 
-                        y++;
-                        showSinglePost(y);
-                        break;
-                    case 2:
-                        likePost(y);
-                        break;
-                    case 3:
-                    int g=0;
-                    break;
-                }
-            
-            }while (g == 0);
+        UserDAO user = new UserDAO();
+        try {
+            int postsnum = user.getPostSize();
+            for (int i = 1; i <= postsnum; i++) {
+                String post = user.getPost(i);
+                System.out.println(post);
+            }
+        } catch (Exception e) {
         }
-
+        
     }
 
+    public void newPost() {
+        System.out.println("Type your Post");
+        String post = in.nextLine();
+        UserDAO user = new UserDAO();
+        try {
+            System.out.println("Loading...");
+            user.addPost(post);
+            System.out.println("Post successfully added");
+        } catch (Exception e) {
+            try {
+                System.out.println(e.getMessage()); 
+            } catch (Exception e1) {
+            }
+        }
+    }
 }
---
+++ UserDAO.java
@@ -0,0 +1,224 @@
+import java.sql.*;
+
+
+/**
+ * UserDAO provides all the necessary methods related to users.
+ * 
+ * @author 
+ *
+ */
+public class UserDAO {
+
+	/**
+	 * This method is used to authenticate a user.
+	 * 
+	 * @param username, String
+	 * @param password, String
+     * @param typeaccount, int
+	 * @return user, the user object
+	 * @throws Exception, if the credentials are not valid
+	 */
+	public User authenticate(String username, String password, int typeaccount) throws Exception {
+		
+		String sql = "SELECT * FROM user WHERE username=? and password=? and typeaccount=?";
+        Connection con = null;
+        DB db = new DB();
+
+        try {
+
+            con = db.getConnection();
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1, username);
+			stmt.setString(2, password);
+            stmt.setInt(3, typeaccount);
+            ResultSet rs = stmt.executeQuery();
+
+            if (!rs.next()) {
+
+                rs.close();
+                stmt.close();
+                db.close();
+                throw new Exception("No User found with username: " + username +"and this password");
+
+            }
+
+            User user = new User(rs.getString("username"),rs.getString("password"),
+                rs.getInt("typeaccount"));
+            rs.close();
+            stmt.close();
+            db.close();
+
+            return user;
+
+            
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        } finally {
+            try {
+                db.close();
+            } catch (Exception e) {
+                
+            }
+        }
+			
+		
+	} //End of authenticate
+	
+	/**
+	 * Register/create new user.
+	 * 
+	 * @param user, user
+	 * @throws Exception, if encounter any error.
+	 */
+	public void register(User user) throws Exception {
+			
+		String sql = "SELECT * FROM user where username=? ;";
+        Connection con = null;
+        DB db = new DB();
+
+        try {
+            
+            con = db.getConnection();
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1, user.getUsername());
+            ResultSet rs = stmt.executeQuery();
+
+			if (rs.next()) {
+
+                rs.close();
+                stmt.close();
+                db.close();
+                throw new Exception("Sorry, username "+ user.getUsername() + " already registered");
+
+            }
+
+			String sql2 = "INSERT INTO user " 
+           		+ " (username,password,typeaccount) VALUES (?, ? ,?);";
+			PreparedStatement stmt2 = con.prepareStatement(sql2);
+			stmt2.setString(1, user.getUsername());
+            stmt2.setString(2, user.getPassword());
+            stmt2.setInt(6, user.getTypeAccount());
+			stmt2.executeUpdate();	
+			stmt2.close();	
+            rs.close();
+            stmt.close();
+            db.close();
+
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        } finally {
+
+            try {
+                db.close();
+            } catch (Exception e) {
+                
+            }
+		
+		}
+	}//end of register
+
+    /**
+	 * adds the post to the posts column of the allposts table
+     * 
+	 * 
+	 * @param user, user
+	 * @throws Exception, if encounter any error.
+	 */
+    public void addPost(String newpost) throws Exception {
+			
+        Connection con = null;
+        DB db = new DB();
+
+        try {
+            
+            con = db.getConnection();
+			String sql = "INSERT INTO allposts(posts) VALUES (?);";
+			PreparedStatement stmt = con.prepareStatement(sql);
+			stmt.setString(1, newpost);
+            stmt.executeUpdate();	
+			stmt.close();
+            db.close();
+
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        } finally {
+
+            try {
+                db.close();
+            } catch (Exception e) {
+                
+            }
+		
+		}
+	}//end of addpost
+	
+    /**
+	 * returns a single post with id = the parameter postid
+	 * @param postid, int
+	 * @throws Exception, if encounter any error.
+	 */
+    public String getPost(int postid) throws Exception {
+			
+        Connection con = null;
+        DB db = new DB();
+
+        try {
+            
+            con = db.getConnection();
+			String sql = "SELECT * FROM allposts where postid=? ;";
+			PreparedStatement stmt = con.prepareStatement(sql);
+			stmt.setInt(1, postid);
+            ResultSet rs = stmt.executeQuery();
+            String post = rs.getString("posts");
+            stmt.close();
+            db.close();
+            return post;
+
+
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        } finally {
+
+            try {
+                db.close();
+            } catch (Exception e) {
+                
+            }
+		
+		}
+	}//end of getPost
+
+
+    /**
+	 * Return size of the allposts table.
+	 * 
+	 * @throws Exception, if encounter any error.
+	 */
+    public int getPostSize() throws Exception {
+        
+        Connection con = null;
+        DB db = new DB();
+
+        try {
+            
+            con = db.getConnection();
+			String sql = "SELECT LAST_INSERT_ID();";
+            PreparedStatement stmt = con.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+            int post = rs.getInt("SELECT LAST_INSERT_ID();");
+            db.close();
+            return post;
+        }  catch (Exception e) {
+            throw new Exception(e.getMessage());
+        } finally {
+
+            try {
+                db.close();
+            } catch (Exception e) {
+                
+            }
+		
+		}
+    }
+
+} //End of class
