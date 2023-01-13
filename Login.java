01c26c15f136f980e67d04f8a158bb52839d41a5
gsklavounakos <116825713+gsklavounakos@users.noreply.github.com>
2023-01-13 20:52:55 +0200

    Update Login.java

--- Login.java
+++ Login.java
@@ -7,24 +7,22 @@ public class Login {
 	public User signInUser() throws Exception {
 		boolean wrongLogin = true;
 		do {
-			try (Scanner in = new Scanner(System.in)) {
-				System.out.println("Username: ");   // enters user's username
-				String username = in.nextLine();
+		try (Scanner in = new Scanner(System.in)) {
+			System.out.println("Username: ");
+			String username = in.nextLine();
 
-				System.out.println("Password: ");   //enters user's password
-				String password = in.nextLine();
+			System.out.println("Password: ");
+			String password = in.nextLine();
 
-				UserDAO userdao = new UserDAO();
+			UserDAO userdao = new UserDAO();
 
-				User thisuser = userdao.authenticate(username, password); //this method is used to authenticate a user
-				wrongLogin = false;
-				return thisuser;
-		        } catch (Exception e) {
-				e.printStackTrace();
-			}
+                    	User thisuser = userdao.authenticate(username, password);
+                    	wrongLogin = false;
+                   	return thisuser;
+            }
 
             
 			
-        	} while(wrongLogin != true); //loop ends when username and password are valid
-	}
+        } while(wrongLogin != true);
+    }
 }
