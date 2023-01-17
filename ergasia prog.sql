10cfb33700070045483edecd9909308d0726889a
gsklavounakos <116825713+gsklavounakos@users.noreply.github.com>
2023-01-17 15:16:02 +0200

    Update ergasia prog.sql

--- ergasia prog.sql
+++ ergasia prog.sql
@@ -1,26 +1,26 @@
 DROP TABLE allcomments;
-DROP TABLE users;
+DROP TABLE allusers;
 DROP TABLE allposts;
 
 
-CREATE TABLE users (
+CREATE TABLE allusers (
    username VARCHAR(30) not null primary key,
    upassword VARCHAR(20) not null,
    typeaccount int not null
 )
 
 CREATE TABLE allposts (
-   postid int not null primary key auto increment,
+   postid int not null primary key identity(1,1),
    post VARCHAR(200),
    rating INT not null,
    likes INT not null,
    commentnumber INT not null,
-   username VARCHAR(30) not null FOREIGN KEY REFERENCES users(username)
+   username VARCHAR(30) not null FOREIGN KEY REFERENCES allusers(username)
 )
 
 CREATE TABLE allcomments (
-   commentid int not null primary key auto increment,
+   commentid int not null primary key identity(1,1),
    comment TEXT not null,
    postid int not null FOREIGN KEY REFERENCES allposts(postid),
-   username VARCHAR(30) not null FOREIGN KEY REFERENCES users(username)
+   username VARCHAR(30) not null FOREIGN KEY REFERENCES allusers(username)
 )
